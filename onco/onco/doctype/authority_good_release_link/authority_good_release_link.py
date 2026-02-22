5968e8d7bc1204ff05bb39e25e46d5b1983553d1
mostafamohamed1984 <mustafanazieer@gmail.com>
2026-02-23 00:37:31 +0200

    adding the missing autohirty good release link child table doctype

---
+++ onco/onco/doctype/authority_good_release_link/authority_good_release_link.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2026, Onco and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class AuthorityGoodReleaseLink(Document):
+	"""Child table to track Authority Good Release documents linked to Incoming Check Report"""
+	pass
