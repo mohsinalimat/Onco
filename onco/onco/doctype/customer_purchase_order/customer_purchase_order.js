0687689e4151ed76b808400dfe25c57f2198391c
mostafamohamed1984 <mustafanazieer@gmail.com>
2026-03-10 20:51:45 +0200

    edits added to the authority good release from the previous live meeting

--- onco/onco/doctype/customer_purchase_order/customer_purchase_order.js
+++ onco/onco/doctype/customer_purchase_order/customer_purchase_order.js
@@ -1,8 +1,28 @@
 // Copyright (c) 2025, ds and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Customer Purchase Order", {
-// 	refresh(frm) {
+frappe.ui.form.on("Customer Purchase Order", {
+	refresh(frm) {
+		// Document uses custom autoname() method in Python
+		// Naming is automatically generated based on order_type and date year
+		// Format: CPO-{TYPE}-{YYYY}-{XXXXX}-{customer_purchase_order_number}
+	},
+	
+	order_type(frm) {
+		// Document uses custom autoname() method based on order_type and date
+		// No need to manually set naming_series as it's handled by Python autoname()
+	},
+	
+	onload(frm) {
+		// Document uses custom autoname() method in Python
+		// Naming is automatically generated on save
+	}
+});
 
-// 	},
-// });
+// Override any Client Script that tries to set naming_series
+// This prevents errors since naming_series field no longer exists
+function set_naming_series(frm) {
+	// Do nothing - naming is handled by Python autoname() method
+	// This function exists only to prevent errors from Client Scripts
+	return;
+}
