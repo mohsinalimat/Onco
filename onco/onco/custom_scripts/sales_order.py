c8e26f7056bef90c4b4a147a1b0a37b5a85d59e2
mostafamohamed1984 <mustafanazieer@gmail.com>
2026-06-27 14:49:20 +0300

    configuring the naming series to the CPO and SO3

--- onco/onco/custom_scripts/sales_order.py
+++ onco/onco/custom_scripts/sales_order.py
@@ -8,8 +8,17 @@ class CustomSalesOrder(SalesOrder):
     def autoname(self):
         from frappe.model.naming import make_autoname
 
-        if self.naming_series:
-            self.name = make_autoname(self.naming_series, self)
+        order_type = self.get("custom_order_type_1")
+        series_map = {
+            "Private Direct Order": "SAL-ORD-PRV-DIR-.YYYY.-.#####",
+            "Private Tenders Order": "SAL-ORD-PRV-TEN-.YYYY.-.#####",
+            "UPA Tender Order": "SAL-ORD-UPA-TEN-.YYYY.-.#####",
+            "UPA Direct Order": "SAL-ORD-UPA-DIR-.YYYY.-.#####",
+            "UPA Distributor Order": "SAL-ORD-UPA-DIS-.YYYY.-.#####",
+        }
+        series = series_map.get(order_type) or self.naming_series
+        if series:
+            self.name = make_autoname(series, self)
         cpo_name = self.get("custom_customer_po")
         if not cpo_name or not self.name:
             return
